22864e22465d232a2badbe561d793f5802a293db
Daniel James <daniel@64studio.com>
2015-07-09 15:34:02 +0100

    Fix sheet size, crop and cross marks in 01_current

--- 01_current.php
+++ 01_current.php
@@ -1,5 +1,5 @@
 <?php
-# THIS ONE HAS AddPageByArray
+# THIS ONE DOES NOT HAVE AddPageByArray ANY MORE :-)
 
 include("../mpdf.php");
 
@@ -8,29 +8,24 @@ $html = substr($html_data, 12, strlen($html_data)-27);
 $style_data = file_get_contents("style_current.css");
 $frontmatter_data = file_get_contents("frontmatter_current.html");
 
-$mpdf = new mPDF();
-// Make it DOUBLE SIDED document
+// Create new PDF with font subsetting, 234mm wide, 297mm high
+$mpdf = new mPDF('s', array(234,297));
+
+// Make it DOUBLE SIDED document with 4mm bleed
 $mpdf->mirrorMargins = 1;
-
 $mpdf->bleedMargin = 4;
-
 
 $mpdf->h2toc = array(); 
 
 $mpdf->WriteHTML($style_data, 1);
 
-$mpdf->AddPageByArray(array('suppress' => 'off'));
-
-$html = "<sethtmlpagefooter name=\"footer-left\" page=\"E\" value=\"on\" />".$html;
-$html = "<sethtmlpagefooter name=\"footer-right\" page=\"O\" value=\"on\" />".$html;
-
 $mpdf->WriteHTML($frontmatter_data, 2);
 
 $mpdf->WriteHTML($html, 2);
 
-$mpdf->SetTitle("Title");
-$mpdf->SetAuthor("Author");
-$mpdf->SetCreator("Booktype");
+$mpdf->SetTitle("An Example Title");
+$mpdf->SetAuthor("Aco");
+$mpdf->SetCreator("Booktype 2.0 and mPDF 6.0");
 
 $mpdf->Output();
 
